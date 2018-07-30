fe0915ce8a37d43ba9772e898a5c75e8949bb067
PC Camp <pccamp@psd.provo.edu>
2018-07-30 11:31:53 -0600

    initial commit

---
+++ Nannr/src/components/suport.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Nannr Support</h1>
+        <div>We're here to help</div>
+      </div>
+    );
+  }
+}
